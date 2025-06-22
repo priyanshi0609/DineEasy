af1b152ca58e98a0da828bd88f17640f9f93c1cd
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-06-23 00:24:56 +0530

    chore:installation done

---
+++ DineEasy/app/_layout.jsx
@@ -0,0 +1,6 @@
+import { Stack } from "expo-router";
+import "../global.css"; // Import global styles 
+
+export default function RootLayout() {
+  return <Stack />;
+}
---
+++ DineEasy/app/index.jsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+    className="bg-red-50"
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        
+        padding: 20,
+        borderColor: "#000",
+      }}
+    >
+      <Text>Hi i am Priyanshi  Both</Text>
+    </View>
+  );
+}
