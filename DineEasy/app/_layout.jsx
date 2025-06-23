2852444b71e589012d6f6b1422e8ece31a2e0306
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-06-23 15:32:56 +0530

    made changes in layout.jsx

--- DineEasy/app/_layout.jsx
+++ DineEasy/app/_layout.jsx
@@ -2,5 +2,10 @@ import { Stack } from "expo-router";
 import "../global.css"; // Import global styles 
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+  <Stack screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="index" />
+    <Stack.Screen name="(tabs)" />
+    </Stack>
+    );
 }
