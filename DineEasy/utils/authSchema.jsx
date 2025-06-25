17319ecafeb489f32ebe50ac7c2fa21c3a35bbb1
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-06-25 17:01:51 +0530

    chore:added signin and signup pages

---
+++ DineEasy/utils/authSchema.jsx
@@ -0,0 +1,14 @@
+import * as Yup from "yup";
+const validationSchema= Yup.object().shape({
+    email:Yup
+    .string()
+    .required("Email is required")
+    .email("Invalid email format"),
+
+    password:Yup
+    .string()
+    .required("Password is required" )
+    .min(6, "Password must be at least 6 characters")
+})
+
+export default validationSchema;
