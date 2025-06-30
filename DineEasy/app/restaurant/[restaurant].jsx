380c5b53b72f107e720a7ee502aaa039f65ea328
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-06-30 21:22:39 +0530

    .

--- DineEasy/app/restaurant/[restaurant].jsx
+++ DineEasy/app/restaurant/[restaurant].jsx
@@ -1,5 +1,5 @@
-import { View,Text } from "react-native";
-import React from "react";
+import { View,Text, Platform, ScrollView } from "react-native";
+import React ,{useState} from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useLocalSearchParams } from "expo-router";
 
@@ -7,16 +7,28 @@ import { useLocalSearchParams } from "expo-router";
 
 export default function Restaurant(){
     const {restaurant} =useLocalSearchParams();
+    const [restaurantData, setRestaurantData] = useState({});
+    const[carouselData, setCarouselData] = useState({});
+    const[slotsData,setSlotsData] = useState({});
+
+
     return(
         <SafeAreaView
            style={[
-            {backgroundColor:"2b2b2b"}
-           ]}>
-            <Text>
+            {backgroundColor:"#2b2b2b" },
+            Platform.OS =="android" && {paddingBottom: 55},
+            Platform.OS =="ios" && {paddingBottom: 20},
+           ]}
+        >
+            <ScrollView className="h-full">
+                <View className="flex-1 my-2 p-2">
+                   <Text className="text-xl text-[#f49b33] mr-2 font-semibold">
                {restaurant}
-            </Text>
-
-        </SafeAreaView>
+            </Text> 
+            <View className="border-b border-[#f49b33] my-2"/>
+                </View>
+            </ScrollView>
+         </SafeAreaView>
            
     )
 }
